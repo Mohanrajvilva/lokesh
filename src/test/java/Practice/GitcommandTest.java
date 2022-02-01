714c49665209151b2ba31d6cc911caa9b94659ca
Mohanrajvilva <mohanrajvilva@gmail.com>
2022-02-01 15:16:16 +0530

    comit message

---
+++ src/test/java/Practice/GitcommandTest.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class GitcommandTest {
+
+	@Test
+	public void gitcommandTest()
+	{
+		System.out.println("First program");
+	}
+}
